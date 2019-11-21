58f97456018ef4516f21b2338950bb4025e6ee37
Slobodan Gak Cavar <slobodan.gak.cavar@gmail.com>
2019-11-21 20:50:01 +0100

    Bootstrap 1

---
+++ main.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+  /* navbar toggler, dodavanje ikonice za hamburger meni */
+
+  $('.navbar-toggler').html("<i class='fas fa-bars fa-3x' style='color:var(--orange)'></i>");
+
+  // smooth scroll
+
+
+});
